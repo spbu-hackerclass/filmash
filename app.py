065259cd2d2c9fddee583e4d77f9a7c7e84dccc6
Gumin Egor <gumin.egor@gmail.com>
2020-11-07 15:37:58 +0300

    Separate db definition

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sqlite3
-
 from flask import Flask, render_template
 from flask import g
 
 app = Flask(__name__)
-
-# Work with SQLite
-DATABASE = 'films.db'
-
-
-# Connect to DB
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
 
 
 # Close handler
